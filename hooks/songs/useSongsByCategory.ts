c0dc37caea4e92cb87f745084765dbb029a723f4
khalid <rabaaouikhalid@gmail.com>
2026-08-05 12:32:09 +0200

    remove bun

---
+++ hooks/songs/useSongsByCategory.ts
@@ -0,0 +1,10 @@
+import { useQuery } from "@tanstack/react-query";
+import { SongService } from "@/services/song.service";
+
+export function useMostListended() {
+  return useQuery({
+    queryKey: ["songs", "trending"],
+
+    queryFn: () => SongService.get(),
+  });
+}
