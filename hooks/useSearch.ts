717d39975c16504d2b03afbf0853f4a7d7ac80fb
khalid <rabaaouikhalid@gmail.com>
2026-08-09 00:55:05 +0200

    fix: sync pnpm lockfile

--- hooks/useSearch.ts
+++ hooks/useSearch.ts
@@ -1,5 +1,4 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
-import { AuthService } from "@/services/auth.service";
 import { SearchDTO } from "@/types";
 import { SearchService } from "@/services/search.service";
 
